c281f1fed66e4ec735a6a1d36ba797f10bfd21f0
unknown <mrtesla07@gmail.com>
2025-11-02 23:24:25 +0300

    Убрать циклический импорт в миграции panel

--- app/migrations/panel.py
+++ app/migrations/panel.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 
 import logging
 
-from .manager import MigrationContext
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .manager import MigrationContext
 
 logger = logging.getLogger(__name__)
 
 
-async def ensure_operator_replied_flag(context: MigrationContext) -> None:
+async def ensure_operator_replied_flag(context: 'MigrationContext') -> None:
     user_ids = await context.storage.get_all_users_ids()
     logger.info("Updating %s users with missing operator_replied flag.", len(user_ids))
     for user_id in user_ids:
